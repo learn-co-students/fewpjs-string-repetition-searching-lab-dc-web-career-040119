08b49c36862fa031aa186de26118c06343dd79cc
Justin <justinhwu95@gmail.com>
2019-05-12 14:27:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude(){
+ var phrase = "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+ return phrase
+}
+
+function prohibitedLanguage(array){
+    return array.search(/candycorn|brusselssprouts/)
+}
