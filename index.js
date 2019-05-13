a392f730de2128f85f4015b0a96b8c27a395e133
errolgh <ewatsonrei@gmail.com>
2019-05-13 00:44:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+function heyJude(){
+  let verse = "Na na na na na na na, na na na na, hey Jude"
+  let prohibitedLanguage = /candycorn|brusselssprouts/;
+  return verse.repeat(16);
+};
+ heyJude();
