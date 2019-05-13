bc9c1b7b39b2af28c6902e5ade1a5edfed7b6d38
errolgh <ewatsonrei@gmail.com>
2019-05-13 16:16:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// Code your solution here
-function heyJude(){
-  let verse = "Na na na na na na na, na na na na, hey Jude"
-  let prohibitedLanguage = /candycorn|brusselssprouts/;
-  return verse.repeat(16);
-};
- heyJude();
+// // Code your solution here
+// function heyJude(){
+//   let verse = "Na na na na na na na, na na na na, hey Jude"
+//   let prohibitedLanguage = /candycorn|brusselssprouts/;
+//   return verse.repeat(16);
+// };
+//  heyJude();
+
+
+let heyJude = (verse) => {
+  // an arrow function
+  return verse.repeat(16)
+}
+
+let prohibitedLanguage = (string) => {
+  let bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);
+}
