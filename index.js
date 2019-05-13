49e6cc6a006abd919ac832d7eac08d1c7c28a935
Marior9 <mariorodriguezan@gmail.com>
2019-05-13 08:27:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+function heyJude(string){
+  return string.repeat(16)
+}
+let bannedWords = /candycorn|brusselssprouts/;
+function prohibitedLanguage(string){
+    return string.search(bannedWords)
+}
